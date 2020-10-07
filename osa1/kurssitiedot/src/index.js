ec4ba78fb745ede74f3477352a614846efac5a98
Kalle <kalle.sissonen@gmail.com>
2020-10-07 19:37:50 +0300

    Tehtävien 1.3-1.5 lopputulos.

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -4,16 +4,17 @@ import ReactDOM from 'react-dom'
 const Header = (props) => {
   return (
     <>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </>
   );
 }
 const Content = (props) => {
+  const parts = props.course.parts;
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <Part part={parts[0].name} exercises={parts[0].exercises} />
+      <Part part={parts[1].name} exercises={parts[1].exercises} />
+      <Part part={parts[2].name} exercises={parts[2].exercises} />
     </div>
   );
 }
@@ -27,29 +28,40 @@ const Part = (props) => {
   );
 }
 const Total = (props) => {
+  const parts = props.course.parts;
   return (
     <>
       <p>
-        Number of exercises {props.total}.
+        Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}.
       </p>
     </>
   );
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content course={course} />
+      <Total course={course} />
     </>
   )
 }
